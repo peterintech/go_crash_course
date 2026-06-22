e8e851874b0fb34496be63d82a6371a25142aeca
peterintech <ogbonnapete210@gmail.com>
2026-06-22 02:02:46 +0100

    fmt guys

--- 01_hello/main.go
+++ 01_hello/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+// fmt stuffs:
+
+// %v — print anything normally
+// %T — check type
+// %d — integers
+// %s — strings
+// %f — floats
+// %t — booleans
+// %+v — debug structs
+// %#v — inspect Go syntax
+// %% — literal percent
+
+// %9f !== %.9f
+
 func main() {
 	fmt.Println("Hello, World!")
 }
